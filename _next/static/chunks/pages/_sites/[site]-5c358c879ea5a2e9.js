68b7269accaa6f36578c4ab289d30686d8a9bd10
galalalmaghraby <107432317+galalalmaghraby@users.noreply.github.com>
2022-10-11 00:58:46 +0200

    project

---
+++ _next/static/chunks/pages/_sites/[site]-5c358c879ea5a2e9.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[378],{27168:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_sites/[site]",function(){return _(43199)}])},43199:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSP:function(){return e}});var t=_(6491),e=!0;u.default=t.default}},function(n){n.O(0,[664,907,94,899,774,888,179],(function(){return u=27168,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
+//# sourceMappingURL=[site]-5c358c879ea5a2e9.js.map
---
+++ _next/static/chunks/pages/index-b889d802bd6a6f6e.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{48312:function(n,u,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return r(5075)}])},5075:function(n,u,r){"use strict";r.r(u),r.d(u,{default:function(){return e}});var t=r(85893);function e(){return(0,t.jsx)("h1",{children:"Home"})}}},function(n){n.O(0,[774,888,179],(function(){return u=48312,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
+//# sourceMappingURL=index-b889d802bd6a6f6e.js.map
